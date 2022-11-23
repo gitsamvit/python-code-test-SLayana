2f8da74d60a8b2f9cff0b9ef03f81488383c4e4d
SLayana <layanashrivastav@gmail.com>
2022-11-23 20:20:25 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -1,46 +1,12 @@
-
 import unittest
 import typewise_alert
 
 
 class TypewiseTest(unittest.TestCase):
     def test_infers_breach_as_per_limits(self):
-        self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == "TOO_LOW")
-
-    def test_infers_breach_as_per_limits1(self):
-        self.assertTrue(typewise_alert.infer_breach(200, 50, 100) == "TOO_HIGH")
-
-    def test_infers_breach_as_per_limits2(self):
-        self.assertTrue(typewise_alert.infer_breach(50, 50, 50) == "NORMAL")
-
-#     def test_send_to_email_low(self):
-#         self.assertFalse(
-#             typewise_alert.send_to_email("TOO_LOW") == ""
-#         )
-
-#     def test_send_to_email_high(self):
-#         self.assertFalse(
-#             typewise_alert.send_to_email("TOO_HIGH")
-#             == ""
-#         )
-    
-    def test_check_and_alert_email(self):
         self.assertFalse(
-            typewise_alert.check_and_alert("TO_EMAIL", "high_active_cooling", 200)
-            == ""
+            typewise_alert.check_and_alert("TO_EMAIL", "passive_cooling", 200) == "the temperature is too low"
         )
-    def test_check_and_alert_email_low(self):
-        self.assertFalse(
-            typewise_alert.check_and_alert("TO_EMAIL", "passive_cooling", -2)
-            == ""
-        )
-
-    def test_check_and_alert_controller(self):
-        self.assertFalse(
-            typewise_alert.check_and_alert("TO_CONTROLLER", "med_active_cooling", 20)
-            == ""
-        )
-
 
 if __name__ == "__main__":
     unittest.main()
