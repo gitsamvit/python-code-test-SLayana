90b977d8a5f7e81378b3671a6414b2d771269a7e
SLayana <layanashrivastav@gmail.com>
2022-11-23 19:36:31 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -32,7 +32,7 @@ class TypewiseTest(unittest.TestCase):
 
     def test_check_and_alert(self):
         self.assertFalse(
-            typewise_alert.check_and_alert("TO_CONTROLLER", "med_active_cooling", 200)
+            typewise_alert.check_and_alert("TO_CONTROLLER", "med_active_cooling", 20)
             == ""
         )
 
